2aa1dca3fd3284c22d8b074eaf2a652fd57b3c76
Fynn Freyer <fynn.freyer@googlemail.com>
2025-02-20 15:29:49 +0100

    build: bump version

--- coloform/__about__.py
+++ coloform/__about__.py
@@ -1,4 +1,4 @@
 __description__ = "Coloform colors terminal output, similar to colorama."
-__version__ = "0.0.1a1"
+__version__ = "0.0.1a2"
 __authors__ = {"name": "Fynn Freyer", "email": "fynn.freyer@googlemail.com"}
 __maintainers__ = __authors__
